a6df2fd631c661ea9766223afbf79886a62ac71e
vinoddsce <vinod.dsce@gmail.com>
2019-11-18 19:50:38 +0530

    Objects and 'this' keyword

--- exe-context.js
+++ exe-context.js
@@ -1,28 +1,68 @@
-var a = 100;
+// var a = 100;
 
-function first() {
-    var firstA = 200;
-    console.log("It's first() function !!!");
+// function first() {
+//     var firstA = 200;
+//     console.log("It's first() function !!!");
 
-    function second() {
-        var secondA = 300;
-        var firstA = 400;
-        console.log("It's second() function !!!");
-        console.log("secondA", secondA);
-        console.log("firstA", firstA);
-        console.log("a", a);
-        console.dir(second);
+//     function second() {
+//         var secondA = 300;
+//         var firstA = 400;
+//         console.log("It's second() function !!!");
+//         console.log("secondA", secondA);
+//         console.log("firstA", firstA);
+//         console.log("a", a);
+//         console.dir(second);
+//     }
+//     second();
+// }
+// // window.first();
+// first();
+// console.log(window);
+
+
+
+///// Objects and 'this' keyword 
+
+
+var someLocal = 200;
+
+var vinod = {
+
+    firstName: 'Vinod',
+    lastName: 'Kumar',
+    yearOfBirth: 1990,
+    isMarried: false,
+    job: 'Teacher',
+
+    calculateAge: function () {
+
+        // var someLocal = 100;
+
+        function getAge() {
+            console.log("getAge()", window.vinod.job);
+        }
+        getAge();
+
+        console.log("Age of ", this.firstName, " is ", (2019 - this.yearOfBirth));
     }
-    second();
-
 }
 
-first();
+// vinod.calculateAge();
 
+var obj = {
+    a: 200
+}
+obj.a = 100;
+console.log(obj.a);
 
+obj = vinod;
+vinod = null;
+obj.calculateAge();
 
-console.log(window);
-
+window.a = 1000;
+console.log(a);
+// console.log([] == ![]);
+// console.log([] === ![]);
 
 
 
