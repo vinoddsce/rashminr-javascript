fcc6e08588428d5904520c114e0288bfaf08dfdd
vinoddsce <vinod.dsce@gmail.com>
2019-11-19 20:04:08 +0530

    Arrow functions introduction

--- arrow-fn.js
+++ arrow-fn.js
@@ -31,12 +31,11 @@ var square = n => n * n;
 
 var yearOfBirth = 2000;
 
-var getYearOfBirth = () => {
-    // this.yearOfBirth;
-    console.log(this);
-}
-
-getYearOfBirth();
+// var getYearOfBirth = () => {
+//     // this.yearOfBirth;
+//     console.log(this);
+// }
+// getYearOfBirth();
 
 var vinod = {
     firstName: 'Vinod',
@@ -44,14 +43,55 @@ var vinod = {
     yearOfBirth: 1990,
     isMarried: false,
     job: 'Teacher',
-
     calculateAge: function () {
         var getAge = (currentYear) => currentYear - this.yearOfBirth;
-        // function getAge(currentYear) {
-        //     return currentYear - this.yearOfBirth;
-        // }
         console.log("Age of ", this.firstName, " is ", getAge(2019));
     }
 }
 
-vinod.calculateAge();
+var kumar = {
+    firstName: 'Vinod1',
+    lastName: 'Kumar1',
+    yearOfBirth: 1999,
+    isMarried: false,
+    job: 'Student',
+}
+
+kumar.calculateAge = vinod.calculateAge;
+
+// console.log(vinod);
+// console.log(kumar.calculateAge());
+// kumar.calculateAge()
+
+var obj = {};
+console.log(obj);
+
+var Person = function (fName, lName, yearOfBirth, isMarried, job) {
+    this.firstName = fName;
+    this.lastName = lName;
+    this.yearOfBirth = yearOfBirth;
+    this.isMarried = isMarried;
+    this.job = job;
+    this.displayFirstName = function () {
+        console.log("First Name: ", this.firstName);
+    };
+    // this.calculateAge = function () {
+    //     var getAge = (currentYear) => currentYear - this.yearOfBirth;
+    //     console.log("Age of ", this.firstName, " is ", getAge(2019));
+    // }
+}
+
+Person.prototype.calculateAge = function () {
+    var getAge = (currentYear) => currentYear - this.yearOfBirth;
+    console.log("Age of ", this.firstName, " is ", getAge(2019));
+}
+
+
+var p1 = new Person('Vinod', 'Kumar', 1990, false, 'Teacher');
+var p2 = new Person('Vinod-2', 'Kumar-2', 1999, false, 'Teacher');
+
+console.log(p1);
+console.log(p2);
+
+
+p1.calculateAge(2019);
