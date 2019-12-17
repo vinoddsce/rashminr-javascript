34fbfa87a1eeae0ce3cac7e8b20231e720c5ff3b
vinoddsce <vinod.dsce@gmail.com>
2019-12-17 20:36:42 +0530

    XMLhttpreq

---
+++ one.js
@@ -0,0 +1,32 @@
+var a = 1;
+
+
+function first() {
+    console.log("first() !!!!!");
+}
+
+
+function third() {
+    setTimeout(() => {
+        console.log("third() !!!!!");
+    }, 1000);
+}
+
+function second() {
+    console.log("second() -> start !!!!!");
+    for (var i = 0; i < 1000000000000; i++) { }
+    console.log("second() -> end !!!!!");
+}
+
+
+
+function fourth() {
+    console.log("fourth() !!!!!");
+}
+
+
+first();
+third();
+second();
+
+fourth();
