408d70e6203470cf4b676d51fb7e9cd99249fdaa
vinoddsce <vinod.dsce@gmail.com>
2019-12-20 20:31:17 +0530

    CommonJS modularity concepts

---
+++ final.js
@@ -0,0 +1,24 @@
+
+const two = require('./two.js');
+
+
+console.log("two : ", two);
+
+console.log("value a is : ", two.twoA);
+
+var x = 100;
+var y = 200;
+
+console.log(`Sum of ${x} and ${y} is : `, two.add(x, y));
+
+
+// console.log(window);
+
+// console.log(global);
+// console.log("Process: ", process);
+
+// setTimeout(() => {
+//     console.log("Hi")
+// }, 2000);
+
+
--- two.js
+++ two.js
@@ -1,1 +1,10 @@
 var a = 100;
+
+exports.twoA = a;
+
+exports.add = function (a, b) {
+    return a + b;
+}
+
+console.log(module.exports);
+
