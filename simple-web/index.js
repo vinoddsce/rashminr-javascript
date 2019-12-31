22505418922f342ac83122db62e5cf30f35ac02a
vinoddsce <vinod.dsce@gmail.com>
2019-12-31 20:37:47 +0530

    Installing dependencies

---
+++ simple-web/index.js
@@ -0,0 +1,9 @@
+const color = require('color')
+
+const redHexa = color({ r: 255, g: 180, b: 180 }).hex()
+
+console.log(redHexa)
+
+
+
+// console.log('My first project !!!!!! Using Scripts !!!!!!');
